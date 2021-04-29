0d569aa9f2aaaddb252ced19cd198c90c761f989
Monaco <monaco@usp.br>
2021-04-29 19:20:51 -0300

    Add eg-06-alt.S

--- eg/hw/egx-01.c
+++ eg/hw/egx-01.c
@@ -1,6 +1,12 @@
-
+/*  A simple example to illustrate how GCC treats both
+    32 and 16-bit codes. */
 
 int __attribute__ ((naked)) foo ()
 {
   return 42;
 }
+
+/* To improve readability, the attribute 'naked' is used to 
+   prevent GCC from cluttering the object code with elements
+   that are not necessary in a flat binary like ours. */
+
