f20f27c243e8c27a49065629ea0638764354a816
Monaco <monaco@usp.br>
2021-04-30 10:53:36 -0300

    Edits

---
+++ eg/hw/egx-04.asm
@@ -0,0 +1,8 @@
+	;; A code excerpt to illustrate NASM vs GAS
+	;; NASM version.
+	
+	bits 16
+	
+	mov al, 0x1
+	mov ax, 0x2
+	mov eax,  0x3
---
+++ eg/hw/egx-07.c
@@ -0,0 +1,28 @@
+/* An exaple to illustrate GCC limitations when
+   dealing with 8-bit registers. */
+
+void __attribute__((naked)) foo()
+{
+
+  register short bx  __asm__   ("bx");
+  register short ax   __asm__  ("ax");
+  register char  ah   __asm__  ("ah");
+  register char  al   __asm__  ("al");
+
+  bx = 0x1;
+  ax = 02;
+  ah = 0x3;
+  al = 0x4; 
+  
+}
+
+
+
+
+
+
+
+
+
+
+
