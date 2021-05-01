0e284187bb54b9800ae34a583708a776dc6d351f
Monaco <monaco@usp.br>
2021-04-30 21:46:17 -0300

    Add eg-05

--- eg/hw/eg-08.c
+++ eg/hw/eg-08.c
@@ -1,50 +1,46 @@
 
-extern const char here[];
+#include <eg-08.h>
 
+extern const char msg[];
 
-/* Reminder: 16bit object code (see note 1). */
-
-/* This will be our entry point.*/
-
-void __attribute__ ((naked)) _start()           
+ 
+void __attribute__((naked)) _start()
 {
 
-  register int eax __asm__  ("eax");                                      
-  register int ebx __asm__  ("ebx");                                      
-                                                                                   
-  eax = 0x0e00;                 /* Load 0xe into ah. */                            
-                                                                                   
-  ebx = 0x0;                    /* Offest to the string. */                        
-                                                                                   
-  do                            
-    {                                                                              
-      eax &= 0xffffff00;        /*     prepare for the next line   */              
-      eax |= (char) here[ebx];  /*     mov al, BYTE [here + bx]    */              
-      __asm__("int     $0x10"); /*     int 0x10                    */              
-      ebx++;                    /*     add bx, 0x1                 */              
-                                                                                   
-    }                                                                              
-  while ( (eax & 0x000000ff)   != 0x0);      /*  while(ah != 0x0)  */    
+  /* Force variables to be allocated in registers rather
+     than in RAM, as usual. */
   
-  while(1);
+  register short ax __asm__("ax"); /* Variable ax is register ax. */
+  register short bx __asm__("bx"); /* Variable bx is register bx. */
 
+  //ax = 0x0e00;	       /* Store 0xe in ah, we don't care about al.*/
+
+  set_h (ax,0x0e);
+  
+  bx = 0x0;	       /* Zero-initilize bx. */
+
+  do
+    {
+
+      /* Gimmick to put msg[bx] in al. */
+      
+      ax = (ax & 0xff00) | (msg[bx] & 0x00ff);
+
+      /* This is output as is in the assembly. */
+
+      __asm__ ("int $0x10");	/* Call BIOS. */
+
+      bx = bx + 1;
+      
+    }
+  while ( (ax & 0x00ff) != 0  ); /* Do while al != 0. */
+
+    while (1)
+      __asm__("hlt");		/* Halt with safeguard. */
 }
 
-const char here[] = "Hello world";
+/*  The string, with a hack to allocate it in the same code section. */
 
+const char msg[]  = "Hello world";
 
-
-
-/* Notes. 
-
-   1) In oder to produce 16-bit program, we may either use the inline assembly
-
-      __asm__ (".code16");
-
-      at the beginning of the file, or else pass the command line option
-      -m16 to gcc, which is absolutely equivalent here. Remember, this 
-      directive needs to be present in the assembly so that the assembly
-      know what to do.
-
-*/
 
