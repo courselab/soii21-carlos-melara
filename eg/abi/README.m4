3df9d338d4264807f0b07296e76c5f5c09616ff1
Monaco <monaco@usp.br>
2021-06-08 16:38:20 -0300

    Add file

---
+++ eg/abi/README.m4
@@ -0,0 +1,25 @@
+include(docm4.m4)
+
+ Application binary interface
+ ==============================
+DOCM4_DIR_NOTICE
+
+ Overview
+ ------------------------------
+
+ This directory contains a series of source code files illustrating different
+ aspects of the x86 application binary interface and related concepts. It
+ includes examples of data alignment, name mangling, syscall interface,
+ calling convention, among others.
+
+ Contents
+ ------------------------------
+
+ * eg-01.c		Simple program to exemplify program sections.
+
+
+ APPENDEX I
+ -----------------------------
+ 
+DOCM4_MAKE_BINTOOLS_DOC
+
---
+++ eg/abi/eg-01.c
@@ -0,0 +1,42 @@
+/* hex2bin.c - Convert a file from hexadecima ascii to binary.
+ 
+   Copyright (c) 2021, Monaco F. J. <monaco@usp.br>
+
+   This file is part of SYSeg.
+
+   SYSeg is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <stdio.h>
+
+int b=1;
+int a;
+
+int main()
+{
+  int c;
+  int d=2;
+
+  a = 3;
+  b = 4;
+  c = 5;
+  d = 6;
+
+
+  printf ("%d %d %d %d\n",a,b,c,d);
+
+  return 0;
+}
+
